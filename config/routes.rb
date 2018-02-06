c39df468fe30e35a4d0a30e51d57d4cfc94229d1
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-06 22:19:03 +0300

    routed routes to about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#index'
+
+  match '/about', to: 'homes#about', via: :get
+  match '/contact', to: 'homes#contact', via: :get
 end
