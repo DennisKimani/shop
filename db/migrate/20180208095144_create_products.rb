dc97fb9953dfbc19bd82be5f1b1d69cc28fd24da
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-08 12:59:48 +0300

    generated resources for products

---
+++ db/migrate/20180208095144_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :details, :string
+      t.column :price, :integer
+      t.column :image, :string
+      t.column :category_id, :integer
+      t.timestamps
+    end
+  end
+end
