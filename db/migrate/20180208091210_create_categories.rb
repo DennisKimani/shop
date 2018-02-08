f8ec68e5e282bee65e60999f9083dce3389f15e4
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-08 12:19:58 +0300

    run rails db:migrate

--- db/migrate/20180208091210_create_categories.rb
+++ db/migrate/20180208091210_create_categories.rb
@@ -1,6 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
+      t.column :name, :string
+      t.column :logo, :string
 
       t.timestamps
     end
