3842e7d2a2892dbb058dee84cf5a8021cedf2944
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-08 13:09:48 +0300

    changed the spelling for one of the columns

--- db/migrate/20180208095144_create_products.rb
+++ db/migrate/20180208095144_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.column :name, :string
-      t.column :details, :string
+      t.column :detail, :string
       t.column :price, :integer
       t.column :image, :string
       t.column :category_id, :integer
