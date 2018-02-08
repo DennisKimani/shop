22baa77799cbca5c88fd78d8f28a5fb9bb15b33c
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-08 13:10:12 +0300

    added the crud functionalities for the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,53 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def show
+    @category = Category.find(params[:category_id])
+    @product = @category.products.find(params[:id])
+    render :show
+  end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @product = @category.products.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @product = @category.products.new(product_params)
+    if @product.save
+      redirect_to category_path(@product.category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :detail, :price, :image)
+  end
 end
