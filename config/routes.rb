cef7046d712f9f77e2fab700c15e7e83a77b1c41
DennisKimani <dennisngugi.dk@gmail.com>
2018-02-08 13:15:30 +0300

    related the products to the categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
   resources :homes
+  resources :categories do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'homes#index'
